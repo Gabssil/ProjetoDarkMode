935425ce132b3149eac327995b2cb8537dce9ad7
Gabriela <126586812+Gabssil@users.noreply.github.com>
2023-10-02 21:24:45 -0300

    Add files via upload

---
+++ atividadePw/script.js
@@ -0,0 +1,11 @@
+function ModoEscuro(button) {
+    var square = document.getElementById("square");
+    square.style.backgroundColor = "#000000";
+    square.style.color = "white";
+  
+    var ModoEscuroAtivo = document.getElementById("txtNome", "txtTelefone", "txtOrigem", "txtDataContato", "txtObservacao" );
+    ModoEscuroAtivo.style.backgroundColor = "#1C1C1C";
+    ModoEscuroAtivo.style.color = "white";
+  }
+  
+  
